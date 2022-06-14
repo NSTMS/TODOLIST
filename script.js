3b3294747b6f0e35cb5010a0ece1e4b330b40d93
nstms <jan.idzi1@gmail.com>
2022-06-14 10:01:25 +0200

    main commit

---
+++ script.js
@@ -0,0 +1,58 @@
+addEventListener("keydown",decectKey)
+
+function decectKey(event)
+{
+   let text = document.getElementById("taskContent").value
+
+    if(event.key == "Enter")
+    {
+            addListElement(text)
+    }
+}
+
+
+let counter = 0
+function addListElement(text)
+{
+    if(text != ""){
+
+        // let itemTitle = document.createElement("div")
+        // itemTitle.classList.add("itemTitle")
+        // itemTitle.innerHTML = `<p>${counter}</p>`
+        // newItem.appendChild(itemTitle)
+
+        let newItem = document.createElement("div")
+        let itemContent = document.createElement("div")        
+        let minus = document.createElement("div")
+        
+        newItem.classList.add("listItem")
+
+        counter++
+        newItem.classList.add(`item-${counter}`)
+        itemContent.classList.add("itemContent")
+
+       
+        itemContent.innerHTML = `<p>${text}</p>`
+        document.getElementById("taskContent").value = ""
+        minus.innerHTML = `<img src="/img/minus.png" alt="minus" onclick="removeElement(${counter})">`
+        itemContent.appendChild(minus)
+
+        newItem.appendChild(itemContent)
+        document.querySelector(".list").appendChild(newItem)
+    }
+}
+
+function deleteLastElement()
+{
+   let listItemArray = document.getElementsByClassName("listItem")
+    if(listItemArray.length != 0)
+    {
+        console.log("usun")
+        listItemArray[length].remove()
+    }
+}
+function removeElement(position)
+{
+    let element = document.querySelector(`.item-${position}`)
+    element.remove()
+}
