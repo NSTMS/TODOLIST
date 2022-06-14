cc022039b54cfb9cc8a3e56490e9fa1976724ebc
nstms <jan.idzi1@gmail.com>
2022-06-14 20:12:41 +0200

    remove all and some more

--- script.js
+++ script.js
@@ -14,45 +14,56 @@ function decectKey(event)
 let counter = 0
 function addListElement(text)
 {
-    if(text != ""){
+    if(text.length < 126)
+    {
+        if(text != ""){
 
-        // let itemTitle = document.createElement("div")
-        // itemTitle.classList.add("itemTitle")
-        // itemTitle.innerHTML = `<p>${counter}</p>`
-        // newItem.appendChild(itemTitle)
-
-        let newItem = document.createElement("div")
-        let itemContent = document.createElement("div")        
-        let minus = document.createElement("div")
-        
-        newItem.classList.add("listItem")
-
-        counter++
-        newItem.classList.add(`item-${counter}`)
-        itemContent.classList.add("itemContent")
-
-       
-        itemContent.innerHTML = `<p>${text}</p>`
+            // let itemTitle = document.createElement("div")
+            // itemTitle.classList.add("itemTitle")
+            // itemTitle.innerHTML = `<p>${counter}</p>`
+            // newItem.appendChild(itemTitle)
+    
+            let newItem = document.createElement("div")
+            let itemContent = document.createElement("div")        
+            let minus = document.createElement("div")
+            
+            newItem.classList.add("listItem")
+    
+            counter++
+            newItem.classList.add(`item-${counter}`)
+            itemContent.classList.add("itemContent")
+    
+           
+            itemContent.innerHTML = `<p onclick="taskDone(${counter})" class="task-${counter}">${text}</p>`
+            document.getElementById("taskContent").value = ""
+            minus.innerHTML = `<img src="https://icons-for-free.com/download-icon-minus+reduce+subtract+icon-1320085760528603434_512.png" alt="minus" onclick="removeElement(${counter})">`
+            itemContent.appendChild(minus)
+    
+            newItem.appendChild(itemContent)
+            document.querySelector(".list").appendChild(newItem)
+        }
+    }else{
+        alert("Za długi teskt!")
         document.getElementById("taskContent").value = ""
-        minus.innerHTML = `<img src="https://cdn-icons.flaticon.com/png/512/1665/premium/1665663.png?token=exp=1655194918~hmac=2797fed9bd102d40f4d0057bd3bc05bag" alt="minus" onclick="removeElement(${counter})">`
-        itemContent.appendChild(minus)
-
-        newItem.appendChild(itemContent)
-        document.querySelector(".list").appendChild(newItem)
     }
+    
 }
 
 function deleteLastElement()
 {
-   let listItemArray = document.getElementsByClassName("listItem")
-    if(listItemArray.length != 0)
-    {
-        console.log("usun")
-        listItemArray[length].remove()
-    }
+    let listItemArray = document.querySelectorAll(".listItem")
+    console.log(listItemArray)
+    Array.prototype.forEach.call( listItemArray, function( node ) {
+        node.parentNode.removeChild( node );
+    });
 }
 function removeElement(position)
 {
     let element = document.querySelector(`.item-${position}`)
     element.remove()
+}
+
+function taskDone(i)
+{
+    document.querySelector(`.task-${i}`).classList.toggle("complete")
 }
